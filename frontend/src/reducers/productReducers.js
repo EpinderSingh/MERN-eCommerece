1ef346026d461e1c882ffaf07f6c56b9dfda3ddd
Epinder <epinder.singh@gmail.com>
2021-06-14 13:23:18 +0800

    adding reducer

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,16 +1,16 @@
 import {
   PRODUCTS_LIST_REQUEST,
-  PRODUCT_LIST_SUCCESS,
-  PRODICT_LIST_FAIL,
+  PRODUCTS_LIST_SUCCESS,
+  PRODUCTS_LIST_FAIL,
 } from '../constants/productConstants';
 
 export const productListReducer = (state = { products: [] }, action) => {
   switch (action.type) {
     case PRODUCTS_LIST_REQUEST:
       return { loading: true, products: [] };
-    case PRODUCT_LIST_SUCCESS:
+    case PRODUCTS_LIST_SUCCESS:
       return { loading: false, products: action.payload };
-    case PRODICT_LIST_FAIL:
+    case PRODUCTS_LIST_FAIL:
       return { loading: false, error: action.payload };
     default:
       return state;
